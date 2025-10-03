c78505cbc093b819f1297f61862ef80ffbdb47fc
Vitor2243 <Vitorsilva4462@gmail.com>
2025-10-03 11:30:04 -0300

    hoje

--- includes/menu.php
+++ includes/menu.php
@@ -16,9 +16,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="post_formulario.php">Incluir Post</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="post_formulario.php">Excluir</a>
-            </li>
             <?php if (isset($_SESSION['login']) 
                 && ($_SESSION['login']['usuario']['adm'] === 1)) : ?>
             <li class="nav-item">
